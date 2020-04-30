049926388c97daea9bed61ac543d7b1dd508f25c
Razvan Popa <razvan@qcade.com>
2020-04-30 15:29:34 -0700

    initial

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+changes=false
+
+mkdir -p charts
+
+sha_root=$(git log --pretty=oneline . | head -n 1 | awk '{print $1}')
+
+for chart in "prometheus-statsd-exporter"; do
+  echo "Checking $chart"
+
+  sha_chart=$(git log --pretty=oneline src/prometheus-statsd-exporter/ | head -n 1 | awk '{print $1}')
+
+
+  if [[ "$sha_chart" == "$sha_root" ]]; then
+    echo "Changes"
+    helm package -d charts ./src/$chart
+    changes=true
+  else
+    echo "No changes"
+  fi
+done
+
+
+if $changes ; then
+  helm repo index --url https://razvvan.github.io/charts/ .
+
+  git fetch --all
+  git checkout gh-pages
+  mv index.yaml charts/
+  git add charts/
+  git commit -m "Chart updates"
+  git push
+fi
+
+
